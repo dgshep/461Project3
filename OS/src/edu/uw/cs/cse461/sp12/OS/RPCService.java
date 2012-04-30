42a3df8b7bf40a8f01d49926014b9da7e722ee37
Matt Hall <mh64161@hotmail.com>
2012-04-30 14:19:34 -0700

    brought spec up to HTTP 1.1

--- OS/src/edu/uw/cs/cse461/sp12/OS/RPCService.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/RPCService.java
@@ -182,6 +182,11 @@ public class RPCService extends RPCCallable {
 						reply.put("host", "");
 						reply.put("callid", json.getInt("id"));
 						reply.put("type", "OK");
+						
+						try{
+							json.getString("connection").equals("keep-alive");
+							reply.put("connection", "keep-alive");
+						} catch (JSONException e) {}
 						handler.sendMessage(reply);
 						id++;
 						handshook = true;
