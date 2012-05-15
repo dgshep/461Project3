0067fbf9e9eb1c5e57e597fb298063e1441add10
Matt Hall <mh64161@hotmail.com>
2012-05-14 20:31:10 -0700

    added ddnsservice

---
+++ OS/src/edu/uw/cs/cse461/sp12/OS/DDNSService.java
@@ -0,0 +1,16 @@
+package edu.uw.cs.cse461.sp12.OS;
+
+public class DDNSService extends RPCCallable {
+
+	@Override
+	public String servicename() {
+		return "ddns";
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
