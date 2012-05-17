f3d21cdc152cb73d0b7c39bc9b4d470ac431c9b0
Davis Shepherd <dgshep@gmail.com>
2012-05-16 18:11:17 -0700

    DDNS Fullname implemented

--- OS/src/edu/uw/cs/cse461/sp12/OS/DDNSFullName.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/DDNSFullName.java
@@ -1,9 +1,18 @@
 package edu.uw.cs.cse461.sp12.OS;
 
 public class DDNSFullName {
+	
+	private String name;
 
-	public DDNSFullName(String string) {
-		// TODO Auto-generated constructor stub
+	public DDNSFullName(String name) {
+		if(!name.endsWith(".")) {
+			this.name = name.trim().concat(".");
+		} else {
+			this.name = name.trim();
+		}
+	}
+	public String toString(){
+		return name;
 	}
 
 }
