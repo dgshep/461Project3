a95e6829a8f7ce3fc2d1f10211cdef4cffb73b00
Matt Hall <mh64161@hotmail.com>
2012-06-01 14:01:58 -0700

    working on fetch updates

--- OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
@@ -8,6 +8,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.uw.cs.cse461.sp12.DB461.DB461.DB461Exception;
+import edu.uw.cs.cse461.sp12.OS.SNetDB461.CommunityRecord;
+import edu.uw.cs.cse461.sp12.OS.SNetDB461.PhotoRecord;
 
 
 
@@ -47,8 +49,21 @@ public class SnetController extends RPCCallable {
 			JSONObject response = sock.invoke("snet", "fetchUpdates", request);
 			JSONObject commUpdates = response.getJSONObject("communityupdates");
 			for(Iterator<String> it = commUpdates.keys(); it.hasNext();) {
-				JSONObject memberUpdate = commUpdates.getJSONObject(it.next());
-				
+				String key = it.next();
+				JSONObject memberUpdate = commUpdates.getJSONObject(key);
+				CommunityRecord cr = db.COMMUNITYTABLE.readOne(key);
+				if(cr.generation < memberUpdate.getInt("generation")) {
+					changeRef(cr.myPhotoHash, -1);
+					changeRef(cr.chosenPhotoHash, -1);
+					int myHash = memberUpdate.getInt("myphotohash");
+					int chosenHash = memberUpdate.getInt("chosenphotohash");
+					cr.generation = memberUpdate.getInt("generation");
+					cr.myPhotoHash = myHash;
+					cr.myPhotoHash = chosenHash;
+					newPhoto(myHash);
+					newPhoto(chosenHash);
+					db.COMMUNITYTABLE.write(cr);
+				}
 			}
 			return response.getJSONArray("photoupdates");
 		} catch (DDNSException e) {
@@ -60,8 +75,29 @@ public class SnetController extends RPCCallable {
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (DB461Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	private void changeRef(int hash, int dif) throws DB461Exception {
+		PhotoRecord pr = db.PHOTOTABLE.readOne(hash);
+		pr.refCount += dif;
+		db.PHOTOTABLE.write(pr);
+	}
+	
+	private void newPhoto(int hash) throws DB461Exception {
+		PhotoRecord pr = db.PHOTOTABLE.readOne(hash);
+		if(pr == null) {
+			pr = db.PHOTOTABLE.createRecord();
+			pr.hash = hash;
+			pr.refCount = 1;
+			pr.file = null;
+		} else {
+			changeRef(hash, 1);
+		}
 	}
 	
 	public byte[] fetchPhoto(String name, int photoHash) {
