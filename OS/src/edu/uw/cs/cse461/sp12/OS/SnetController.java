226246c7b973b67bbbc03bfcaf870f820e40bd0d
Davis Shepherd <dgshep@gmail.com>
2012-05-31 18:38:05 -0700

    Committing project file

--- OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
@@ -1,6 +1,18 @@
 package edu.uw.cs.cse461.sp12.OS;
 
 public class SnetController extends RPCCallable {
+
+	@Override
+	public String servicename() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
