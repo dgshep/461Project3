8dda378d19780e71a273884c7176a88aad60ef1e
Matt Hall <mh64161@hotmail.com>
2012-06-01 15:51:14 -0700

    some shit

--- OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
@@ -1,7 +1,10 @@
 package edu.uw.cs.cse461.sp12.OS;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +18,7 @@ import edu.uw.cs.cse461.sp12.OS.SNetDB461.PhotoRecord;
 
 public class SnetController extends RPCCallable {
 	
+	//TODO friends
 	private SNetDB461 db;
 	
 	public SnetController() {
@@ -42,12 +46,17 @@ public class SnetController extends RPCCallable {
 	}
 	
 	public JSONArray fetchUpdates(String name) {
+		//TODO Make neededphotos
+		
 		try {
 			DDNSRRecord rr = ((DDNSResolverService)OS.getService("ddnsresolver")).resolve(name);
 			RPCCallerSocket sock = new RPCCallerSocket(rr.host, rr.host, rr.port);
 			JSONObject request = new JSONObject();
+			//TODO fill out request: {community:{MemberField,...}, needphotos:[int, ...]}
+			//Put community table into JSON, Make neededphotos
 			JSONObject response = sock.invoke("snet", "fetchUpdates", request);
 			JSONObject commUpdates = response.getJSONObject("communityupdates");
+			
 			for(Iterator<String> it = commUpdates.keys(); it.hasNext();) {
 				String key = it.next();
 				JSONObject memberUpdate = commUpdates.getJSONObject(key);
@@ -65,6 +74,7 @@ public class SnetController extends RPCCallable {
 					db.COMMUNITYTABLE.write(cr);
 				}
 			}
+			
 			return response.getJSONArray("photoupdates");
 		} catch (DDNSException e) {
 			// TODO failed ddns for whatever reason
@@ -100,9 +110,57 @@ public class SnetController extends RPCCallable {
 		}
 	}
 	
-	public byte[] fetchPhoto(String name, int photoHash) {
+	public List<File> getUnusedPhotos(){
+		try{
+			List<File> result = new LinkedList<File>();
+			for(PhotoRecord pr : db.PHOTOTABLE.readAll()) {
+				if(pr.refCount == 0) {
+					result.add(pr.file);
+				}
+				db.PHOTOTABLE.delete(pr.hash);
+			}
+			return result;
+		} catch (DB461Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
+	public byte[] fetchPhoto(String name, int photoHash) {
+		DDNSRRecord rr;
+		try {
+			rr = ((DDNSResolverService)OS.getService("ddnsresolver")).resolve(name);
+			RPCCallerSocket sock = new RPCCallerSocket(rr.host, rr.host, rr.port);
+			JSONObject request = new JSONObject();
+		} catch (DDNSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void setPhotoLocation(int hash, File location) {
+		try {
+			PhotoRecord pr = db.PHOTOTABLE.readOne(hash);
+			pr.file = location;
+			db.PHOTOTABLE.write(pr);
+		} catch (DB461Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
+//	public List<String> getOnlineUsers() {
+//		List<String> names = new LinkedList<String>();
+//		
+//		return names;
+//	}
+	
 }
