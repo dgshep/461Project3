966a1ccc7dc04231dc76c245189bd0a5e1b4ee2e
Matt Hall <mh64161@hotmail.com>
2012-06-01 12:59:34 -0700

    working on the fetchupdate method

--- OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
+++ OS/src/edu/uw/cs/cse461/sp12/OS/SnetController.java
@@ -1,5 +1,10 @@
 package edu.uw.cs.cse461.sp12.OS;
 
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.uw.cs.cse461.sp12.DB461.DB461.DB461Exception;
@@ -7,6 +12,7 @@ import edu.uw.cs.cse461.sp12.DB461.DB461.DB461Exception;
 
 
 public class SnetController extends RPCCallable {
+	
 	private SNetDB461 db;
 	
 	public SnetController() {
@@ -33,11 +39,32 @@ public class SnetController extends RPCCallable {
 		return null;
 	}
 	
-	public void fetchUpdates(String name) {
-		
+	public JSONArray fetchUpdates(String name) {
+		try {
+			DDNSRRecord rr = ((DDNSResolverService)OS.getService("ddnsresolver")).resolve(name);
+			RPCCallerSocket sock = new RPCCallerSocket(rr.host, rr.host, rr.port);
+			JSONObject request = new JSONObject();
+			JSONObject response = sock.invoke("snet", "fetchUpdates", request);
+			JSONObject commUpdates = response.getJSONObject("communityupdates");
+			for(Iterator<String> it = commUpdates.keys(); it.hasNext();) {
+				JSONObject memberUpdate = commUpdates.getJSONObject(it.next());
+				
+			}
+			return response.getJSONArray("photoupdates");
+		} catch (DDNSException e) {
+			// TODO failed ddns for whatever reason
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
-	public byte[] fetchPhoto(int photoHash) {
+	public byte[] fetchPhoto(String name, int photoHash) {
 		return null;
 	}
 	
