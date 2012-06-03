be7d838846ef122b5f5d294aa35d7847d673923b
Davis Shepherd <dgshep@gmail.com>
2012-06-02 23:30:09 -0700

    Can set myPhoto

--- AndroidSnet/src/cse461/snet/MainSnetActivity.java
+++ AndroidSnet/src/cse461/snet/MainSnetActivity.java
@@ -1,6 +1,9 @@
 package cse461.snet;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
 import edu.uw.cs.cse461.sp12.DB461.ContextManager;
@@ -9,17 +12,20 @@ import edu.uw.cs.cse461.sp12.OS.DDNSFullName;
 import edu.uw.cs.cse461.sp12.OS.DDNSResolverService;
 import edu.uw.cs.cse461.sp12.OS.OS;
 import edu.uw.cs.cse461.sp12.OS.RPCService;
+import edu.uw.cs.cse461.sp12.OS.SNetDB461.Photo;
 import edu.uw.cs.cse461.sp12.OS.SnetController;
 import edu.uw.cs.cse461.sp12.util.Log;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainSnetActivity extends Activity {
 	
@@ -46,6 +52,17 @@ public class MainSnetActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		externalStorage = Environment.getExternalStorageDirectory();
+		mCam = (Button) findViewById(R.id.camera);
+		mCam.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+				
+			}
+			
+		});
 	}
 
 	private void bootOS(){
@@ -56,10 +73,11 @@ public class MainSnetActivity extends Activity {
 		config.put("ddns.rootserver", "cse461.cs.washington.edu");
 		config.put("ddns.rootport", "46150");
 		config.put("ddns.password", "champ");
-		config.put("rpc.serverport", "0");
-		config.put("host.name", "hallshep.cse461");
+		config.put("rpc.serverport", "46150");
+		config.put("host.name", "hallshep.cse461.");
 		config.put("ddns.resolvesteps","10");
-		config.put("ddns.namespace", "hallshep.cse461");
+		config.put("ddns.namespace", "hallshep.cse461.");
+		config.put("hallshep.cse461.", "SOA");
 		
 		// boot the OS and load RPC services
 		ContextManager.setContext(getApplicationContext());
@@ -76,27 +94,39 @@ public class MainSnetActivity extends Activity {
 			throw new IllegalStateException("OS Hasn't booted!");
 		}
 		snet = (SnetController) OS.getService("snet");
+		snet.setPhotoDirectory(externalStorage);
 		
 		
-		mCam = (Button) findViewById(R.id.camera);
-		mCam.setOnClickListener(new OnClickListener(){
-
-			public void onClick(View v) {
-				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		
+		myPhoto = (ImageView) findViewById(R.id.myPhoto);
+		File myPhotoFile = new File(externalStorage, "myPhoto.png");
+		if(myPhotoFile.exists()) {
+			try {
+				myPhoto.setImageBitmap(BitmapLoader.loadBitmap(myPhotoFile.getCanonicalPath(), 500, 700));
+			} catch (IOException e) {
 				
 			}
-			
-		});
-		myPhoto = (ImageView) findViewById(R.id.myPhoto);
+		}
 		
 		
 	}
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
-		if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE){
+		bootOS();
+		if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK){
 			Bitmap photoBmp = (Bitmap)data.getExtras().get("data");
 			myPhoto.setImageBitmap(photoBmp);
-			
+			OutputStream outStream = null;
+			File file = new File(externalStorage, "myPhoto.png");
+			try {
+			    outStream = new FileOutputStream(file);
+			    photoBmp.compress(Bitmap.CompressFormat.PNG, 75, outStream);
+			    outStream.flush();
+			    outStream.close();
+			    snet.setMyPhoto(new Photo(file));
+			    Log.i("Community", snet.toString());
+			} catch(IOException e) {
+				Toast.makeText(MainSnetActivity.this, "Failed to Save Image", Toast.LENGTH_SHORT).show();
+			}
 		}
 	}
 	protected void onStart(){
@@ -104,7 +134,6 @@ public class MainSnetActivity extends Activity {
 		bootOS();
 	}
     
-
 
     protected void onStop(){
     	super.onStop();
